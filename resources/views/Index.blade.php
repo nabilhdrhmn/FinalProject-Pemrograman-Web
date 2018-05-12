887a225d936f3a87c4e13e60ab8dda6d61d4b04c
falahrazzaq <falahaththaariqrazzaq@gmail.com>
2018-05-12 13:07:36 +0700

    nambah logo

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <header>
+            <h1><img href="index.php" src="/resources/assets/logo.png"></h1>
+            <nav>
+                <ul>
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="About.php">About</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+
+</body>
+</html>
