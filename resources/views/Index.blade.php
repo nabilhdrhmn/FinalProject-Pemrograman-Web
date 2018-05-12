bc9ad1bb4ce2dca4ae61c7c5fd97f32c83e0182d
falahrazzaq <falahaththaariqrazzaq@gmail.com>
2018-05-12 15:39:12 +0700

    di sesuain ke laptop ku sikit

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
 <head>
     <title></title>
 </head>
 <body>
     <header>
-            <h1><img href="index.php" src="/resources/assets/logo.png"></h1>
+            <h1><img href="index.php" src={{url('logo.png')}}></h1>
             <nav>
                 <ul>
                     <li class="active"><a href="index.php">Home</a></li>
